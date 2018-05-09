6915716e7fbee2588eebb9009cb68d37a0d5c8d0
Ubuntu <ubuntu@ip-172-31-39-205.us-east-2.compute.internal>
2018-05-09 09:41:36 +0000

    removed menu links

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 		 
+	def self.from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+		  user.provider = auth.provider
+		  user.uid = auth.uid
+		  user.password = Devise.friendly_token[0,20]
+		  if ["facebook"].include?(auth.provider)
+			user.save(:validate => false)
+		  else
+			user.email = auth.info.email
+			user.save
+		  end
+		end
+	  end
 end
