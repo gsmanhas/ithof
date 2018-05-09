95c2c1f236205b33a51e5fd2c2ce048d7f2a33e6
Ubuntu <ubuntu@ip-172-31-39-205.us-east-2.compute.internal>
2018-05-09 21:07:28 +0000

    calculate site visits

--- app/controllers/plainpage_controller.rb
+++ app/controllers/plainpage_controller.rb
@@ -6,5 +6,15 @@ class PlainpageController < ApplicationController
     # flash[:warn ] = "Israel don't quite like warnings"
     #flash[:danger ] = "Naomi let the dog out!"
   end
-
+	if cookies.has_key?(:unique_hit_key)
+    hit = Hit.find(cookies[:unique_hit_key])
+    hit.count = hit.count.to_i+1
+    hit.save
+  else
+    cookies[:unique_hit_key] = {
+      :value => Hit.create(count: 1).id,
+      :expires => 1.year.from_now,
+      :domain => 'warm-refuge-51314.herokuapp.com'
+    }
+  end
 end
